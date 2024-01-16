2d83dfc0e69d5edf3da3cf7300a6e73cb7ae31b3
Your Name <you@example.com>
2024-01-16 09:49:23 -0800

    commit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
-"""Module for task 0"""
+"""
+Queries reddit api and returns number of subscribers
+"""
+
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers
-    to the subreddit"""
-    import requests
-
-    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code >= 300:
+    headers = {
+        "User-Agent": "RedditSubscribers/1.0"
+        }
+    url = "https://api.reddit.com/r/{}/about".format(subreddit)
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code != 200:
+            return 0
+        data = response.json()
+        subscribers = data.get("data").get("subscribers")
+        return subscribers
+    except Exception as e:
         return 0
-
-    return sub_info.json().get("data").get("subscribers")
